2c12397881e88505754b79787cb420494849e213
vagner.franco <vagner.franco@technobox.com.br>
2021-04-24 23:29:06 -0300

    fix of comments

--- src/core/useCases/allocatePIDAtMemorySlotAddress.php
+++ src/core/useCases/allocatePIDAtMemorySlotAddress.php
@@ -5,6 +5,5 @@ use DB1OS\core\repository\MemorySlotAddress;
 function allocatePIDAtMemorySlotAddress(
     string $processPID,
     MemorySlotAddress $memorySlotAddress,
-    array $memoryPool // array of arrays where each represents a row and its inner
-    positions the columns of the memory pool matrix
+    array $memoryPool // array of arrays where each represents a row and its inner positions the columns of the memory pool matrix
 ): MemoryPool {}
