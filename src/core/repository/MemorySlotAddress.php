19ad79d8af83a3b9c3bf145273e31a564ece9143
vagner.franco <vagner.franco@technobox.com.br>
2021-04-24 23:27:40 -0300

    use case allocatePIDAtMemorySlotAddress

--- src/core/repository/MemorySlotAddress.php
+++ src/core/repository/MemorySlotAddress.php
@@ -1,4 +1,6 @@
 <?php
+namespace DB1OS\core\repository;
+use DB1OS\core\entity\UnsignedInteger;
 interface MemorySlotAddress {
     public function getRow(): UnsignedInteger;
     public function getColumn(): UnsignedInteger;
